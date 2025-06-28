cb016b2b61aa83be41b2c1704a1cae8ef1f1529a
Jeanlopez935 <jcjlopezsalas@gmail.com>
2025-06-28 17:46:41 -0400

    post update

--- blog/urls.py
+++ blog/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from .views import (PostListView,
                     PostDetailView,
                     PostCreateView,
+                    PostUpdateView,
                     )
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post-list'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/create/', PostCreateView.as_view(), name='post-create'),
+    path('post/update/<int:pk>/', PostUpdateView.as_view(), name='post-update'),
 ]
